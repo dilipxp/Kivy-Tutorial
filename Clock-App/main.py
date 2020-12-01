066f7bbce85b0fc3ec0a9fe11199bf07da8fadc7
Dilip Singh <43517895+kevinstaroo7@users.noreply.github.com>
2020-12-01 11:42:38 +0530

    new function added
    start_stop() and reset funtion added,
    clock not ready yet

--- Clock-App/main.py
+++ Clock-App/main.py
@@ -36,24 +36,30 @@ class RobotoButton():
 
 
 class ClockApp(App):
+    sw_started = False
     sw_seconds = 0
 
-    def update(self, nap):
-        self.sw_seconds += nap
+    def update_clock(self, nap):
+        if self.sw_started:
+          self.sw_seconds += nap
 
     def update_time(self, nap):
         self.root.ids.time.text = time.strftime('[b]%I[/b]:%M:%S')
-        self.sw_seconds += nap
+       # self.sw_seconds += nap
         minutes, seconds = divmod(self.sw_seconds, 60)
         self.root.ids.stopwatch.text = ('%02d:%02d.[size=40]%02d[/size]' %(int(minutes), int(seconds),int(seconds * 100 % 100)))
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
 
+
     def start_stop(self):
         self.root.ids.start_stop.text = ('Start'
-        if self.sw_started else 'Stop')
+            if self.sw_started else 'Stop')
         self.sw_started = not self.sw_started
+
+
+
     def reset(self):
         if self.sw_started:
             self.root.ids.start_stop.text = 'Start'
